3336c08f2b49fe34f13b8d6a35dfb7666a216010
Jose Carreiro <jmcgisc@gmail.com>
2025-06-21 23:38:14 -0600

    testimonios

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,    // p.ej. https://xyz.supabase.co
-  process.env.REACT_APP_SUPABASE_URL // la anon public key
+  process.env.REACT_APP_SUPABASE_ANON_KEY // la anon public key
 );
