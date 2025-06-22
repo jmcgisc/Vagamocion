c8591e021510d6f348b853292afb953a062f4afb
Jose Carreiro <jmcgisc@gmail.com>
2025-06-21 22:17:42 -0600

    imagen

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL,    // p.ej. https://xyz.supabase.co
-  process.env.SUPABASE_KEY // la anon public key
+  process.env.REACT_APP_SUPABASE_URL,    // p.ej. https://xyz.supabase.co
+  process.env.REACT_APP_SUPABASE_URL // la anon public key
 );
