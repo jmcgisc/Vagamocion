31a8e34fd357e6730946501ca5d04ad3ef95f380
Jose Carreiro <jmcgisc@gmail.com>
2025-06-21 19:23:49 -0600

    imagen

--- src/pages/NuevaReseña.jsx
+++ src/pages/NuevaReseña.jsx
@@ -79,13 +79,13 @@ export default function NuevaReseña({ onPublicado }) {
         </h2>
 
         {/* Nombre */}
-        <label className="block mb-2 font-medium">Servicio recibido *</label>
+        <label className="block mb-2 font-medium">Nombre *</label>
         <input
           type="text"
           className="w-full p-2 mb-4 border rounded"
           placeholder="Ej. Vuelo, Hotel, Tour..."
-          value={servicio}
-          onChange={(e) => setServicio(e.target.value)}
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
         />
 
         {/* Servicio */}
